4152058060644451039781a379bb36be126b8491
Ibrahim-Badwi <ib.a.badwi@gmail.com>
2019-10-26 11:58:05 +0300

    Add postgre to databses.

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -131,6 +131,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'localdb': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tajruba',
+        'USER': 'tajruba',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
